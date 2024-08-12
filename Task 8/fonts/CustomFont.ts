b5dc94fa4f38ce643e471c1afdbb11df4edf3698
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-08-12 11:59:46 +0300

    Add files via upload

---
+++ Task 8/fonts/CustomFont.ts
@@ -0,0 +1,10 @@
+import { Poppins ,Epilogue } from "next/font/google";
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700", "900"],
+});
+export const epilogue = Epilogue({
+  subsets: ["latin"],
+  weight: ["400", "600", "700", "900"],
+});
---
+++ Task 8/types/otpFormData.ts
@@ -0,0 +1,3 @@
+export interface otpFormData {
+  otp: string;
+}
